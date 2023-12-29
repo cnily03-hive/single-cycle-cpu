747aaf64f678b55317d6352582bd51bfa5ba819d
Cnily03 <cnily03@outlook.com>
2023-12-29 22:17:18 +0800

    feat: allow to use `+` as number

--- transform.js
+++ transform.js
@@ -266,11 +266,13 @@ function asm2bin(input_asm_lines, options = {
      */
     const parseAsmNum = (asm_num_string) => {
         str = asm_num_string.trim().toLowerCase()
-        if (/^#-?\d+$/i.test(str)) return parseInt(str.slice(1), 10)
+        if (/^#[+-]?\d+$/i.test(str)) return parseInt(str.slice(1), 10)
 
         let sign = 1
         if (/^-/i.test(str)) {
             sign = -1
+            str = str.slice(1)
+        } else if (/^\+/i.test(str)) {
             str = str.slice(1)
         }
 
